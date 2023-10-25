97c4557d03ffd834ff967201e43f5675a7f0e73f
damian-radzio <drado2003@gmail.com>
2023-10-25 18:21:21 +0200

    scripts refactor

--- dist/game.js
+++ dist/game.js
@@ -3,10 +3,14 @@ const numberOfBoardFields = 20;
 const maxPointOfDice = 6;
 const diceList = [];
 const boardFieldsList = [];
-const specialFields = [12, 19];
 let drawnNumber;
 let activeField = 1;
 const instruction = document.createElement('div');
+var GameStatusTypes;
+(function (GameStatusTypes) {
+    GameStatusTypes["WIN"] = "WIN";
+    GameStatusTypes["DEFEAT"] = "DEFEAT";
+})(GameStatusTypes || (GameStatusTypes = {}));
 (function () {
     const board = document.getElementById('board');
     let field = document.createElement('div');
@@ -22,6 +26,7 @@ const instruction = document.createElement('div');
         boardFieldsList.push(field);
     }
     const firstField = document.getElementById('field-1');
+    firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('first-field', 'active-field', 'jumpedOn');
     const startImage = document.createElement('img');
     startImage.src = 'src/assets/start.png';
     firstField === null || firstField === void 0 ? void 0 : firstField.appendChild(startImage);
@@ -29,9 +34,6 @@ const instruction = document.createElement('div');
     const finishImage = document.createElement('img');
     finishImage.src = 'src/assets/finish.png';
     lastField === null || lastField === void 0 ? void 0 : lastField.appendChild(finishImage);
-    firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('first-field');
-    firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('active-field');
-    firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('jumpedOn');
     lastField === null || lastField === void 0 ? void 0 : lastField.classList.add('last-field');
     // special fields
     const specialFields = [
@@ -48,23 +50,21 @@ const instruction = document.createElement('div');
     });
     for (let i = 1; i <= maxPointOfDice; i++) {
         const dice = document.createElement('div');
-        dice.classList.add('dice');
-        dice.classList.add(`dice-${i}`);
+        dice.classList.add('dice', `dice-${i}`);
         document.body.appendChild(dice);
         for (let x = 1; x <= i; x++) {
             const diceDot = document.createElement('div');
-            diceDot.classList.add('dice-dot');
-            diceDot.classList.add(`dot-${x}`);
+            diceDot.classList.add('dice-dot', `dot-${x}`);
             dice.appendChild(diceDot);
         }
         diceList.push(dice);
     }
     instruction.innerHTML = `
-<h2>INSTRUKCJA</h2>
-<p>1. Uwazaj na pole 12! Wejdziesz? Przegrywasz!</p>
-<p>2. Uwazaj na pole 19! Wejdziesz? Cofasz się do pola o numerze 11!</p>
-<p>3. Gra kończy się wejściem na pole 20</p>
-<p>4. Uwaga! Jeśli przekroczysz pole mety cofasz się z niej o tyle pól o ile ją przekroczyłeś!</p>
+    <h2>INSTRUKCJA</h2>
+    <p>1. Uwazaj na pole 12! Wejdziesz? Przegrywasz!</p>
+    <p>2. Uwazaj na pole 19! Wejdziesz? Cofasz się do pola o numerze 11!</p>
+    <p>3. Gra kończy się wejściem na pole 20</p>
+    <p>4. Uwaga! Jeśli przekroczysz pole mety cofasz się z niej o tyle pól o ile ją przekroczyłeś!</p>
   `;
     instruction.classList.add('instruction');
     document.body.appendChild(instruction);
@@ -72,9 +72,54 @@ const instruction = document.createElement('div');
     gameInfo.classList.add('game-info');
     document.body.appendChild(gameInfo);
 })();
+const handleShowGameStatus = (gameStatus, boardValues) => {
+    const restartGameBtn = document.createElement('button');
+    const gameInfo = document.querySelector('.game-info');
+    const setGameStatus = () => {
+        const dices = document.querySelectorAll('.dice');
+        const handleResetGame = () => {
+            boardFieldsList.forEach((el) => el.classList.remove('jumpedOn'));
+            const firstField = document.getElementById('field-1');
+            firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('first-field');
+            firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('active-field');
+            firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('jumpedOn');
+            instruction.classList.remove('hidden');
+            dices.forEach((dice) => dice.classList.remove('hidden'));
+            document.body.classList.remove(boardValues.bodyClassName);
+            gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.removeChild(gameInfoText);
+            gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.removeChild(restartGameBtn);
+            gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.removeChild(gameInfoStats);
+            activeField = 1;
+            restartGameBtn.removeEventListener('click', handleResetGame, true);
+        };
+        dices.forEach((dice) => dice.classList.add('hidden'));
+        instruction.classList.add('hidden');
+        restartGameBtn.classList.add('restart-game-btn');
+        restartGameBtn.textContent = boardValues.restartGameBtnText;
+        const gameInfoText = document.createElement('p');
+        gameInfoText.textContent = boardValues.gameInfoText;
+        document.body.classList.add(boardValues.bodyClassName);
+        const gameInfoStats = document.createElement('div');
+        gameInfoStats.classList.add('game-stats');
+        gameInfoStats.innerHTML = `
+    <h2>STATYSTYKI GRY</h2>
+    <p>łączna liczba rzutów kostką: ${gameStats.totalNumberOfThrows}</p>
+    <p>Średnia liczba wyrzuconych oczek: ${(gameStats.totalNumberOfStitches / gameStats.totalNumberOfThrows).toPrecision(2)}</p>
+    `;
+        gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.appendChild(gameInfoText);
+        gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.appendChild(restartGameBtn);
+        gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.appendChild(gameInfoStats);
+        restartGameBtn.addEventListener('click', handleResetGame, true);
+    };
+    setGameStatus();
+};
 const gameStats = {
     totalNumberOfStitches: 0,
     totalNumberOfThrows: 0,
+};
+const updateGameStats = () => {
+    gameStats.totalNumberOfThrows++;
+    gameStats.totalNumberOfStitches += drawnNumber;
 };
 const gameBoardStatusDEFEAT = {
     gameInfoText: 'Przegrałeś... Aby rozpocząć od nowa kliknij przycisk',
@@ -99,6 +144,46 @@ const goToField = (fieldNumber) => {
         boardFieldsList.forEach((el) => el.classList.remove('jumpedOn'));
         boardFieldsList[activeField - 1].classList.add('jumpedOn');
     }, 500);
+};
+const checkSpecialFields = () => {
+    setTimeout(() => {
+        if (activeField === 12) {
+            bodyWarningAlarm();
+            setTimeout(() => {
+                handleShowGameStatus(GameStatusTypes.DEFEAT, gameBoardStatusDEFEAT);
+            }, 2000);
+        }
+        else if (activeField === 20)
+            return handleShowGameStatus(GameStatusTypes.WIN, gameBoardStatusWIN);
+        else if (activeField === 19) {
+            bodyWarningAlarm();
+            setTimeout(() => {
+                goToField(11);
+            }, 2000);
+        }
+    }, drawnNumber * 500);
+};
+const handleBlockDice = () => {
+    diceList.forEach((el) => el.classList.remove('active-dice'));
+    setTimeout(() => {
+        diceList.map((dice) => {
+            if (dice.className.includes(`${drawnNumber}`)) {
+                dice.classList.add('active-dice', 'blocked-dice');
+                setTimeout(() => {
+                    dice.classList.remove('blocked-dice');
+                }, 1000 + 400 * drawnNumber);
+            }
+        });
+    }, 100);
+};
+const handleRollDice = () => {
+    drawnNumber = generatePointOfDice(1, maxPointOfDice);
+    handleBlockDice();
+    setTimeout(() => {
+        handleMovePawn();
+    }, 500);
+    updateGameStats();
+    return drawnNumber;
 };
 const handleMovePawn = () => {
     let animationDelayMultiplier = 1;
@@ -129,91 +214,8 @@ const handleMovePawn = () => {
     else if (activeField + drawnNumber === 19) {
         goToField(11);
     }
-    setTimeout(() => {
-        if (activeField === 12) {
-            bodyWarningAlarm();
-            setTimeout(() => {
-                handleShowGameStatus('DEFEAT', gameBoardStatusDEFEAT);
-            }, 2000);
-        }
-        else if (activeField === 20)
-            return handleShowGameStatus('WIN', gameBoardStatusWIN);
-        else if (activeField === 19) {
-            bodyWarningAlarm();
-            setTimeout(() => {
-                goToField(11);
-            }, 2000);
-        }
-    }, drawnNumber * 500);
+    checkSpecialFields();
     diceList.forEach((el) => el.removeEventListener('click', handleRollDice));
-};
-const handleRollDice = () => {
-    let drawnDice;
-    diceList.forEach((el) => el.classList.remove('active-dice'));
-    drawnNumber = generatePointOfDice(1, maxPointOfDice);
-    const handleBlockDice = (dice) => {
-        dice.classList.add('blocked-dice');
-        setTimeout(() => {
-            dice.classList.remove('blocked-dice');
-        }, 1000 + 400 * drawnNumber);
-    };
-    setTimeout(() => {
-        diceList.map((dice) => {
-            if (dice.className.includes(`${drawnNumber}`)) {
-                dice.classList.add('active-dice');
-                handleBlockDice(dice);
-            }
-        });
-    }, 100);
-    setTimeout(() => {
-        handleMovePawn();
-    }, 500);
-    gameStats.totalNumberOfThrows++;
-    gameStats.totalNumberOfStitches += drawnNumber;
-    return drawnNumber;
-};
-const handleShowGameStatus = (gameStatus, boardValues) => {
-    const restartGameBtn = document.createElement('button');
-    const gameInfo = document.querySelector('.game-info');
-    const setGameStatus = () => {
-        const dices = document.querySelectorAll('.dice');
-        const handleResetGame = () => {
-            boardFieldsList.forEach((el) => el.classList.remove('jumpedOn'));
-            const firstField = document.getElementById('field-1');
-            firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('first-field');
-            firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('active-field');
-            firstField === null || firstField === void 0 ? void 0 : firstField.classList.add('jumpedOn');
-            instruction.classList.remove('hidden');
-            dices.forEach((dice) => dice.classList.remove('hidden'));
-            document.body.classList.remove(boardValues.bodyClassName);
-            gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.removeChild(gameInfoText);
-            gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.removeChild(restartGameBtn);
-            gameStats.totalNumberOfStitches = 0;
-            gameStats.totalNumberOfThrows = 0;
-            activeField = 1;
-            restartGameBtn.removeEventListener('click', handleResetGame, true);
-        };
-        dices.forEach((dice) => dice.classList.add('hidden'));
-        instruction.classList.add('hidden');
-        restartGameBtn.classList.add('restart-game-btn');
-        restartGameBtn.textContent = boardValues.restartGameBtnText;
-        const gameInfoText = document.createElement('p');
-        gameInfoText.textContent = boardValues.gameInfoText;
-        document.body.classList.add(boardValues.bodyClassName);
-        const gameInfoStats = document.createElement('div');
-        gameInfoStats.classList.add('game-stats');
-        gameInfoStats.innerHTML = `
-    <h2>STATYSTYKI GRY</h2>
-    <p>łączna liczba rzutów kostką: ${gameStats.totalNumberOfThrows}</p>
-    <p>Średnia liczba wyrzuconych oczek: ${(gameStats.totalNumberOfStitches / gameStats.totalNumberOfThrows).toPrecision(2)}</p>
-    `;
-        gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.appendChild(gameInfoText);
-        gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.appendChild(restartGameBtn);
-        gameInfo === null || gameInfo === void 0 ? void 0 : gameInfo.appendChild(gameInfoStats);
-        console.log(gameStats);
-        restartGameBtn.addEventListener('click', handleResetGame, true);
-    };
-    setGameStatus();
 };
 if (diceList.findIndex((el) => el.className.includes('active-dice')) === -1) {
     diceList[0].classList.add('active-dice');
--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,14 @@
 const numberOfBoardFields: number = 20;
-const maxPointOfDice = 6;
+const maxPointOfDice: number = 6;
 const diceList: Array<HTMLElement> = [];
 const boardFieldsList: Array<HTMLElement> = [];
-const specialFields = [12, 19];
 let drawnNumber: number;
-let activeField = 1;
-type GameStatus = 'WIN' | 'DEFEAT';
+let activeField: number = 1;
 const instruction = document.createElement('div');
+enum GameStatusTypes {
+  WIN = 'WIN',
+  DEFEAT = 'DEFEAT',
+}
 
 (function () {
   const board = document.getElementById('board');
@@ -27,6 +29,8 @@ const instruction = document.createElement('div');
   }
 
   const firstField = document.getElementById('field-1');
+  firstField?.classList.add('first-field', 'active-field', 'jumpedOn');
+
   const startImage = document.createElement('img');
   startImage.src = 'src/assets/start.png';
   firstField?.appendChild(startImage);
@@ -37,10 +41,6 @@ const instruction = document.createElement('div');
   const finishImage = document.createElement('img');
   finishImage.src = 'src/assets/finish.png';
   lastField?.appendChild(finishImage);
-
-  firstField?.classList.add('first-field');
-  firstField?.classList.add('active-field');
-  firstField?.classList.add('jumpedOn');
   lastField?.classList.add('last-field');
 
   // special fields
@@ -59,26 +59,25 @@ const instruction = document.createElement('div');
 
   for (let i = 1; i <= maxPointOfDice; i++) {
     const dice = document.createElement('div');
-    dice.classList.add('dice');
-    dice.classList.add(`dice-${i}`);
+    dice.classList.add('dice', `dice-${i}`);
     document.body.appendChild(dice);
 
     for (let x = 1; x <= i; x++) {
       const diceDot = document.createElement('div');
-      diceDot.classList.add('dice-dot');
-      diceDot.classList.add(`dot-${x}`);
+      diceDot.classList.add('dice-dot', `dot-${x}`);
       dice.appendChild(diceDot);
     }
     diceList.push(dice);
   }
 
   instruction.innerHTML = `
-<h2>INSTRUKCJA</h2>
-<p>1. Uwazaj na pole 12! Wejdziesz? Przegrywasz!</p>
-<p>2. Uwazaj na pole 19! Wejdziesz? Cofasz się do pola o numerze 11!</p>
-<p>3. Gra kończy się wejściem na pole 20</p>
-<p>4. Uwaga! Jeśli przekroczysz pole mety cofasz się z niej o tyle pól o ile ją przekroczyłeś!</p>
+    <h2>INSTRUKCJA</h2>
+    <p>1. Uwazaj na pole 12! Wejdziesz? Przegrywasz!</p>
+    <p>2. Uwazaj na pole 19! Wejdziesz? Cofasz się do pola o numerze 11!</p>
+    <p>3. Gra kończy się wejściem na pole 20</p>
+    <p>4. Uwaga! Jeśli przekroczysz pole mety cofasz się z niej o tyle pól o ile ją przekroczyłeś!</p>
   `;
+
   instruction.classList.add('instruction');
   document.body.appendChild(instruction);
 
@@ -86,6 +85,61 @@ const instruction = document.createElement('div');
   gameInfo.classList.add('game-info');
   document.body.appendChild(gameInfo);
 })();
+
+const handleShowGameStatus = (
+  gameStatus: GameStatusTypes,
+  boardValues: GameBoardStatus
+): void => {
+  const restartGameBtn = document.createElement('button');
+  const gameInfo = document.querySelector('.game-info');
+
+  const setGameStatus = () => {
+    const dices = document.querySelectorAll('.dice');
+
+    const handleResetGame = () => {
+      boardFieldsList.forEach((el) => el.classList.remove('jumpedOn'));
+      const firstField = document.getElementById('field-1');
+      firstField?.classList.add('first-field');
+      firstField?.classList.add('active-field');
+      firstField?.classList.add('jumpedOn');
+      instruction.classList.remove('hidden');
+      dices.forEach((dice) => dice.classList.remove('hidden'));
+      document.body.classList.remove(boardValues.bodyClassName);
+
+      gameInfo?.removeChild(gameInfoText);
+      gameInfo?.removeChild(restartGameBtn);
+      gameInfo?.removeChild(gameInfoStats);
+
+      activeField = 1;
+      restartGameBtn.removeEventListener('click', handleResetGame, true);
+    };
+
+    dices.forEach((dice) => dice.classList.add('hidden'));
+    instruction.classList.add('hidden');
+    restartGameBtn.classList.add('restart-game-btn');
+    restartGameBtn.textContent = boardValues.restartGameBtnText;
+    const gameInfoText = document.createElement('p');
+    gameInfoText.textContent = boardValues.gameInfoText;
+    document.body.classList.add(boardValues.bodyClassName);
+    const gameInfoStats = document.createElement('div');
+    gameInfoStats.classList.add('game-stats');
+    gameInfoStats.innerHTML = `
+    <h2>STATYSTYKI GRY</h2>
+    <p>łączna liczba rzutów kostką: ${gameStats.totalNumberOfThrows}</p>
+    <p>Średnia liczba wyrzuconych oczek: ${(
+      gameStats.totalNumberOfStitches / gameStats.totalNumberOfThrows
+    ).toPrecision(2)}</p>
+    `;
+
+    gameInfo?.appendChild(gameInfoText);
+    gameInfo?.appendChild(restartGameBtn);
+    gameInfo?.appendChild(gameInfoStats);
+
+    restartGameBtn.addEventListener('click', handleResetGame, true);
+  };
+
+  setGameStatus();
+};
 
 type GameBoardStatus = {
   gameInfoText: string;
@@ -95,6 +149,10 @@ type GameBoardStatus = {
 const gameStats = {
   totalNumberOfStitches: 0,
   totalNumberOfThrows: 0,
+};
+const updateGameStats = () => {
+  gameStats.totalNumberOfThrows++;
+  gameStats.totalNumberOfStitches += drawnNumber;
 };
 
 const gameBoardStatusDEFEAT: GameBoardStatus = {
@@ -108,7 +166,7 @@ const gameBoardStatusWIN: GameBoardStatus = {
   bodyClassName: 'body-end-game-win',
 };
 
-const bodyWarningAlarm = () => {
+const bodyWarningAlarm = (): void => {
   document.body.classList.add('warning');
   setTimeout(() => {
     document.body.classList.remove('warning');
@@ -118,7 +176,7 @@ const bodyWarningAlarm = () => {
 const generatePointOfDice = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
-const goToField = (fieldNumber: number) => {
+const goToField = (fieldNumber: number): void => {
   activeField = fieldNumber;
   setTimeout(() => {
     boardFieldsList.forEach((el) => el.classList.remove('jumpedOn'));
@@ -126,7 +184,49 @@ const goToField = (fieldNumber: number) => {
   }, 500);
 };
 
-const handleMovePawn = () => {
+const checkSpecialFields = (): void => {
+  setTimeout(() => {
+    if (activeField === 12) {
+      bodyWarningAlarm();
+      setTimeout(() => {
+        handleShowGameStatus(GameStatusTypes.DEFEAT, gameBoardStatusDEFEAT);
+      }, 2000);
+    } else if (activeField === 20)
+      return handleShowGameStatus(GameStatusTypes.WIN, gameBoardStatusWIN);
+    else if (activeField === 19) {
+      bodyWarningAlarm();
+      setTimeout(() => {
+        goToField(11);
+      }, 2000);
+    }
+  }, drawnNumber * 500);
+};
+
+const handleBlockDice = (): void => {
+  diceList.forEach((el) => el.classList.remove('active-dice'));
+  setTimeout(() => {
+    diceList.map((dice) => {
+      if (dice.className.includes(`${drawnNumber}`)) {
+        dice.classList.add('active-dice', 'blocked-dice');
+        setTimeout(() => {
+          dice.classList.remove('blocked-dice');
+        }, 1000 + 400 * drawnNumber);
+      }
+    });
+  }, 100);
+};
+
+const handleRollDice = (): number => {
+  drawnNumber = generatePointOfDice(1, maxPointOfDice);
+  handleBlockDice();
+  setTimeout(() => {
+    handleMovePawn();
+  }, 500);
+  updateGameStats();
+  return drawnNumber;
+};
+
+const handleMovePawn = (): void => {
   let animationDelayMultiplier = 1;
   if (activeField + drawnNumber <= numberOfBoardFields) {
     for (let i = activeField; i <= activeField + drawnNumber - 1; i++) {
@@ -154,106 +254,9 @@ const handleMovePawn = () => {
     goToField(11);
   }
 
-  setTimeout(() => {
-    if (activeField === 12) {
-      bodyWarningAlarm();
-      setTimeout(() => {
-        handleShowGameStatus('DEFEAT', gameBoardStatusDEFEAT);
-      }, 2000);
-    } else if (activeField === 20)
-      return handleShowGameStatus('WIN', gameBoardStatusWIN);
-    else if (activeField === 19) {
-      bodyWarningAlarm();
-      setTimeout(() => {
-        goToField(11);
-      }, 2000);
-    }
-  }, drawnNumber * 500);
+  checkSpecialFields();
+
   diceList.forEach((el) => el.removeEventListener('click', handleRollDice));
-};
-
-const handleRollDice = () => {
-  let drawnDice: HTMLCollection;
-
-  diceList.forEach((el) => el.classList.remove('active-dice'));
-  drawnNumber = generatePointOfDice(1, maxPointOfDice);
-
-  const handleBlockDice = (dice: HTMLElement) => {
-    dice.classList.add('blocked-dice');
-    setTimeout(() => {
-      dice.classList.remove('blocked-dice');
-    }, 1000 + 400 * drawnNumber);
-  };
-
-  setTimeout(() => {
-    diceList.map((dice) => {
-      if (dice.className.includes(`${drawnNumber}`)) {
-        dice.classList.add('active-dice');
-        handleBlockDice(dice);
-      }
-    });
-  }, 100);
-
-  setTimeout(() => {
-    handleMovePawn();
-  }, 500);
-  gameStats.totalNumberOfThrows++;
-  gameStats.totalNumberOfStitches += drawnNumber;
-  return drawnNumber;
-};
-
-const handleShowGameStatus = (
-  gameStatus: GameStatus,
-  boardValues: GameBoardStatus
-): void => {
-  const restartGameBtn = document.createElement('button');
-  const gameInfo = document.querySelector('.game-info');
-
-  const setGameStatus = () => {
-    const dices = document.querySelectorAll('.dice');
-
-    const handleResetGame = () => {
-      boardFieldsList.forEach((el) => el.classList.remove('jumpedOn'));
-      const firstField = document.getElementById('field-1');
-      firstField?.classList.add('first-field');
-      firstField?.classList.add('active-field');
-      firstField?.classList.add('jumpedOn');
-      instruction.classList.remove('hidden');
-      dices.forEach((dice) => dice.classList.remove('hidden'));
-      document.body.classList.remove(boardValues.bodyClassName);
-      gameInfo?.removeChild(gameInfoText);
-      gameInfo?.removeChild(restartGameBtn);
-      gameStats.totalNumberOfStitches = 0;
-      gameStats.totalNumberOfThrows = 0;
-      activeField = 1;
-      restartGameBtn.removeEventListener('click', handleResetGame, true);
-    };
-
-    dices.forEach((dice) => dice.classList.add('hidden'));
-    instruction.classList.add('hidden');
-    restartGameBtn.classList.add('restart-game-btn');
-    restartGameBtn.textContent = boardValues.restartGameBtnText;
-    const gameInfoText = document.createElement('p');
-    gameInfoText.textContent = boardValues.gameInfoText;
-    document.body.classList.add(boardValues.bodyClassName);
-    const gameInfoStats = document.createElement('div');
-    gameInfoStats.classList.add('game-stats');
-    gameInfoStats.innerHTML = `
-    <h2>STATYSTYKI GRY</h2>
-    <p>łączna liczba rzutów kostką: ${gameStats.totalNumberOfThrows}</p>
-    <p>Średnia liczba wyrzuconych oczek: ${(
-      gameStats.totalNumberOfStitches / gameStats.totalNumberOfThrows
-    ).toPrecision(2)}</p>
-    `;
-
-    gameInfo?.appendChild(gameInfoText);
-    gameInfo?.appendChild(restartGameBtn);
-    gameInfo?.appendChild(gameInfoStats);
-    console.log(gameStats);
-    restartGameBtn.addEventListener('click', handleResetGame, true);
-  };
-
-  setGameStatus();
 };
 
 if (diceList.findIndex((el) => el.className.includes('active-dice')) === -1) {
